1090222526e8d6456d4bbb9bc690a4791481b617
MAHTMATCIS <tongmx33@163.com>
2025-02-07 18:31:27 +0800

    a

--- netlify/functions/hello-world/hello-world.mjs
+++ netlify/functions/hello-world/hello-world.mjs
@@ -1,15 +1,32 @@
-// Docs on request and context https://docs.netlify.com/functions/build/#code-your-function-2
-export default (request, context) => {
+export default async (request, context) => {
   try {
-    const url = new URL(request.url)
-    const type = request.type
-    const method = request.method;
-    const context1 = request.value
+    if (request.method === 'POST') {
+      const { formData } = await request.formData(); // 获取POST请求的表单数据
+      let postData = {}; // 初始化一个空对象来存储表单数据
 
-    return new Response(`Hello! 200 ${type} ${url} ${method} ${context} ${context1}`)
+      // 遍历formData中的条目并将它们添加到postData对象中
+      for (const [key, value] of formData.entries()) {
+        postData[key] = value;
+      }
+
+      // 返回POST数据作为JSON响应
+      return new Response(JSON.stringify(postData), {
+        headers: { 'Content-Type': 'application/json' },
+      });
+    } else if(request.method === 'GET') {
+      // 如果不是POST请求，返回一个提示信息
+      return new Response(`<h1>Hello, Everybody!</h1><div>Thank you to visit this page <u>"Far from the distance".</u></div><div><p style="font-family: 'Arial Black',serif ; color: red ; text-underline: red"><u>BUT</u></p> this page isn't any public page. So you'd better go away <p style="font-family: 'Arial Black',serif ; color: red ; text-underline: red; font-size: 40px"><u>NOW</u></p></div>`, {
+        status: 405, // Method Not Allowed
+      });
+    }else{
+        // 如果不是POST请求，返回一个提示信息
+        return new Response(`This endpoint only accepts POST requests.`, {
+          status: 405, // Method Not Allowed
+        });
+    }
   } catch (error) {
     return new Response(error.toString(), {
       status: 500,
-    })
+    });
   }
-}
+};
