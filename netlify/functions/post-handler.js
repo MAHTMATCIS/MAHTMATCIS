5bb99ceef0953c1dc57d3e820ab608e548025518
MAHTMATCIS <tongmx33@163.com>
2025-02-08 11:33:13 +0800

    post-handler.js new

--- netlify/functions/post-handler.js
+++ netlify/functions/post-handler.js
@@ -1,4 +1,5 @@
 const {getStore} = require("@netlify/blobs");
+const {response} = require("express");
 
 const siteID = '11c8ee6b-b6e3-40e2-ac72-d318b943066f'; // 替换为实际的Site ID
 const token = 'nfp_QiY2SXh1EsPKHR9wYRnRXFz6KDtheXYfcce2'; // 替换为实际的API Token
@@ -6,6 +7,14 @@ const token = 'nfp_QiY2SXh1EsPKHR9wYRnRXFz6KDtheXYfcce2'; // 替换为实际的A
 
 exports.handler = async (event) => {
   const { httpMethod, body } = event;
+  function parseUserData(data) {
+    let datas=data.split('\n');
+    let result=[];
+    for(let i=0; i<datas.length; i++) {
+      result.push(JSON.parse(datas[i]))
+    }
+    return result
+  }
 
   if (httpMethod === 'POST') {
     try {
@@ -13,42 +22,57 @@ exports.handler = async (event) => {
       const parsedBody = JSON.parse(body);
       console.log('Received POST request with body:', parsedBody);
 
+      if (parsedBody.type==="register"){
+        const storage=getStore({
+            name:'deploy',
+            siteID:siteID,
+            token:token
+        });
 
-      const storage=getStore({
-          name:'deploy',
-          siteID:siteID,
-          token:token
-      });
 
+        let data = {
+          username: parsedBody.username,
+          password: parsedBody.password,
+          data: ""
+        };
 
-      let data = {
-        username: parsedBody.username,
-        password: parsedBody.password
-      };
-      data=JSON.stringify(data);
-      let from=storage.get('userData');
-      from=JSON.stringify(from);
-      console.log(`data from the storage`,from,'type of:',typeof from);
-      data=from+"\n"+data;
+        data=JSON.stringify(data);
+        let from=await storage.get('userData');
 
-      await storage.set('userData', data);
-      console.log(`File uploaded successfully. Blob data: ${data}`);
+        let datas=parseUserData(from);
+        for (let i=0; i<datas.length; i++) {
+          if (datas[i].username===parsedBody.username)
+            return {
+              statusCode: 400, body: JSON.stringify({type:"error", message: 'Register failure', data: 'User already exists!' })
+            }
+        }
 
-      return {
-        statusCode: 200,
-        body: JSON.stringify({ message: 'User data recorded successfully' })
-      };
+        console.log(`data from the storage`,from,'type of:',typeof from);
+
+        if (from !== null){
+          data=from+"\n"+data;
+        }
+        await storage.set('userData', data);
+        console.log(`File uploaded successfully. Blob data: ${data}`);
+
+        return {
+          statusCode: 200,
+          body: JSON.stringify({type:"info",  message: 'User data recorded successfully', data: `User registered successfully. Data:"${data}"` })
+        };
+      }else if(parsedBody.type==="login"){
+
+      }
     } catch (error) {
       console.error('Error processing the request:', error);
       return {
         statusCode: 400,
-        body: JSON.stringify({ error: 'Error processing the request' })
+        body: JSON.stringify({type:"error",  message: 'Error processing the request' ,data:error.toString()})
       };
     }
   } else {
     return {
       statusCode: 405, // Method Not Allowed
-      body: JSON.stringify({ error: 'Only POST method is allowed for user login' })
+      body: JSON.stringify({type:"error",  message: 'Only POST method is allowed for user login' ,data:'null'})
     };
   }
 };
