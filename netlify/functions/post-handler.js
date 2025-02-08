20aeb4d10e99a85e6deac37fa090e49fffcc36c4
MAHTMATCIS <tongmx33@163.com>
2025-02-08 10:20:25 +0800

    post-handler.js

--- netlify/functions/post-handler.js
+++ netlify/functions/post-handler.js
@@ -1,4 +1,14 @@
 const {getStore} = require("@netlify/blobs");
+const { createClient } = require('@netlify/blobs');
+
+const siteID = '11c8ee6b-b6e3-40e2-ac72-d318b943066f'; // 替换为实际的Site ID
+const token = 'nfp_QiY2SXh1EsPKHR9wYRnRXFz6KDtheXYfcce2'; // 替换为实际的API Token
+
+const store = createClient({
+  siteID,
+  token,
+});
+
 
 exports.handler = async (event) => {
   const { httpMethod, body } = event;
@@ -10,9 +20,9 @@ exports.handler = async (event) => {
       console.log('Received POST request with body:', parsedBody);
 
 
-      const construction = getStore("construction");
+      const result = await store.put('deploy',{username: parsedBody.username,  password: parsedBody.password});
+      console.log(`File uploaded successfully. Blob URL: ${result.url}`);
 
-      await construction.setJSON("nails", { type: "common", finish: "bright" });
 
 
       return {
