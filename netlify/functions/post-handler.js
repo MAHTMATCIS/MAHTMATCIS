d48e58b3d724bf9daf741b7ee5204b312a8fdd07
MAHTMATCIS <tongmx33@163.com>
2025-02-08 11:46:06 +0800

    post-handler.js new

--- netlify/functions/post-handler.js
+++ netlify/functions/post-handler.js
@@ -1,5 +1,4 @@
 const {getStore} = require("@netlify/blobs");
-const {response} = require("express");
 
 const siteID = '11c8ee6b-b6e3-40e2-ac72-d318b943066f'; // 替换为实际的Site ID
 const token = 'nfp_QiY2SXh1EsPKHR9wYRnRXFz6KDtheXYfcce2'; // 替换为实际的API Token
@@ -8,10 +7,12 @@ const token = 'nfp_QiY2SXh1EsPKHR9wYRnRXFz6KDtheXYfcce2'; // 替换为实际的A
 exports.handler = async (event) => {
   const { httpMethod, body } = event;
   function parseUserData(data) {
-    let datas=data.split('\n');
     let result=[];
-    for(let i=0; i<datas.length; i++) {
-      result.push(JSON.parse(datas[i]))
+    if (data!=null){
+      let datas=data.split('\n');
+      for(let i=0; i<datas.length; i++) {
+        result.push(JSON.parse(datas[i]))
+      }
     }
     return result
   }
@@ -60,7 +61,29 @@ exports.handler = async (event) => {
           body: JSON.stringify({type:"info",  message: 'User data recorded successfully', data: `User registered successfully. Data:"${data}"` })
         };
       }else if(parsedBody.type==="login"){
-
+        const storage=getStore({
+          name:'deploy',
+          siteID:siteID,
+          token:token
+        });
+        let from=await storage.get('userData');
+        let flg = false;
+        let data = null;
+        let datas=parseUserData(from);
+        for (let i=0; i<datas.length; i++) {
+          if (datas[i].username===parsedBody.username && datas[i].password===parsedBody.password) {
+            flg=true;
+            data=datas.data;
+          }
+        }
+        if (!flg)return{
+          statusCode: 400,
+          body: JSON.stringify({type:"error",  message: 'Login failure' ,data:"Check username and password failure!"})
+        };
+        return{
+          statusCode: 200,
+          body: JSON.stringify({type:"info",  message: 'Login susses' ,data:data})
+        };
       }
     } catch (error) {
       console.error('Error processing the request:', error);
