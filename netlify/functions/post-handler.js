035d9ebc61d9c184d9f96aa1faf48c248bffce01
MAHTMATCIS <tongmx33@163.com>
2025-02-08 16:10:45 +0800

    Final v0.0.2fix-06

--- netlify/functions/post-handler.js
+++ netlify/functions/post-handler.js
@@ -6,6 +6,13 @@ const app = express();
 //
 app.use(cors());
 
+const headers={
+  "Access-Control-Allow-Origin" : "*",
+  "Access-Control-Allow-Methods" : "GET, POST, PUT, DELETE, OPTIONS",
+  "Access-Control-Allow-Headers":"Content-Type, Authorization, content-type, authorization, Accept, Authorization, Content-Type, Content-Length, X-CSRF-Token, Token, session, Origin, Host, Connection, Accept-Encoding, Accept-Language, X-Requested-With, Authorization, Content-Type, X-Requested-With, Accept, Accept-Language, Origin, Cache-Control, Pragma, Expires, Range, DNT, X-XSRF-TOKEN, X-Traceid, X-Gseq, Traceid, Token, X-Language-Category, x-timestamp, yxw-accounttype, yxw-secretid, _env_name_, X-Tenant-Id, X-Appid, X-D2, X-Aid, X-Token, X-Token-Type, X-Nonce, X-Platform, X-Sign, X-Ts, X-App-Version, X-Sign-Retry, X-Auto-Test, X-Aid-Skey, X-Login-Type, X-Corp-Uin",
+  "Content-Type": "application/json",
+}
+
 
 const siteID = '11c8ee6b-b6e3-40e2-ac72-d318b943066f'; // 替换为实际的Site ID
 const token = 'nfp_QiY2SXh1EsPKHR9wYRnRXFz6KDtheXYfcce2'; // 替换为实际的API Token
@@ -53,12 +60,7 @@ exports.handler = async (event) => {
             return {
               statusCode: 200, body: JSON.stringify({type:"error", message: 'Register failure', data: 'User already exists!' }),
 
-              headers:{
-                "Access-Control-Allow-Origin" : "*",
-                "Access-Control-Allow-Methods" : "GET, POST, PUT, DELETE, OPTIONS",
-                "Access-Control-Allow-Headers":"Content-Type, Authorization, content-type, authorization",
-                "Content-Type": "application/json",
-              }}
+              headers:headers}
         }
 
         console.log(`data from the storage`,from,'type of:',typeof from);
@@ -73,12 +75,7 @@ exports.handler = async (event) => {
           statusCode: 200,
           body: JSON.stringify({type:"info",  message: 'User data recorded successfully', data: `User registered successfully. Data:"${data}"` }),
 
-          headers:{
-            "Access-Control-Allow-Origin" : "*",
-            "Access-Control-Allow-Methods" : "GET, POST, PUT, DELETE, OPTIONS",
-            "Access-Control-Allow-Headers":"Content-Type, Authorization, content-type, authorization",
-            "Content-Type": "application/json",
-          }};
+          headers:headers};
       }else if(parsedBody.type==="login"){
         const storage=getStore({
           name:'deploy',
@@ -98,22 +95,12 @@ exports.handler = async (event) => {
         if (!flg)return{
           statusCode: 200,
           body: JSON.stringify({type:"error",  message: 'Login failure' ,data:"Check username and password failure!"}),
-          headers:{
-            "Access-Control-Allow-Origin" : "*",
-            "Access-Control-Allow-Methods" : "GET, POST, PUT, DELETE, OPTIONS",
-            "Access-Control-Allow-Headers":"Content-Type, Authorization, content-type, authorization",
-            "Content-Type": "application/json",
-          }
+          headers:headers
         };
         return{
           statusCode: 200,
           body: JSON.stringify({type:"info",  message: 'Login susses' ,data:data}),
-          headers:{
-            "Access-Control-Allow-Origin" : "*",
-            "Access-Control-Allow-Methods" : "GET, POST, PUT, DELETE, OPTIONS",
-            "Access-Control-Allow-Headers":"Content-Type, Authorization, content-type, authorization",
-            "Content-Type": "application/json",
-          }
+          headers:headers
         };
       }
     } catch (error) {
@@ -121,36 +108,21 @@ exports.handler = async (event) => {
       return {
         statusCode: 400,
         body: JSON.stringify({type:"error",  message: 'Error processing the request' ,data:error.toString()}),
-        headers:{
-          "Access-Control-Allow-Origin" : "*",
-          "Access-Control-Allow-Methods" : "GET, POST, PUT, DELETE, OPTIONS",
-          "Access-Control-Allow-Headers":"Content-Type, Authorization, content-type, authorization",
-          "Content-Type": "application/json",
-        }
+        headers:headers
       };
     }
 
   } else if (httpMethod==='OPTIONS'){
     return {
       statusCode: 200,
-      headers:{
-        "Access-Control-Allow-Origin" : "*",
-        "Access-Control-Allow-Methods" : "GET, POST, PUT, DELETE, OPTIONS",
-        "Access-Control-Allow-Headers":"Content-Type, Authorization, content-type, authorization",
-        "Content-Type": "application/json",
-      }
+      headers:headers
     }
   }
   else  {
     return {
       statusCode: 405, // Method Not Allowed
       body: JSON.stringify({type:"error",  message: 'Only POST method is allowed for user login' ,data:'null'}),
-      headers:{
-        "Access-Control-Allow-Origin" : "*",
-        "Access-Control-Allow-Methods" : "GET, POST, PUT, DELETE, OPTIONS",
-        "Access-Control-Allow-Headers":"Content-Type, Authorization, content-type, authorization",
-        "Content-Type": "application/json",
-      }
+      headers:headers
     };
   }
 };
